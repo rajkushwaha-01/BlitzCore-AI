07d43eb62860929314e544a43d8ba27e22548cd4
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-07-09 13:42:02 +0530

    Add chat functionality with AI response generation and routing

---
+++ Backend/src/controllers/chat.controller.js
@@ -0,0 +1,36 @@
+import { generateResponse  , generateChatTitle} from '../services/ai.service.js';
+import chatModel from '../models/chat.model.js';
+import messageModel from '../models/message.model.js';  
+
+
+
+export async function sendMessage(req, res) {
+  try {
+    const { message } = req.body;
+    const title = await generateChatTitle(message);
+    const result = await generateResponse(message);
+    
+
+    const chat = await chatModel.create({
+      user : req.user.id,
+      title
+      
+      
+    });
+
+    const  aiMessage = await messageModel.create({
+      chat : chat._id,
+      content : result,
+      role : 'ai'
+    });
+
+
+
+
+
+    res.status(200).json({title , chat , aiMessage});
+    }catch (error) {
+    console.error('Error sending message:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+}
--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,17 +1,40 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-
+import {ChatMistralAI} from "@langchain/mistralai";
+import { HumanMessage  , SystemMessage} from "@langchain/core/messages";
 
 const geminiModel = new ChatGoogleGenerativeAI({
-    model: "gemini-flash-latest",
-    apiKey: process.env.GEMINI_API_KEY
+  model: "gemini-2.5-flash",
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
+const mistralModel = new ChatMistralAI({
+  model: "mistral-small-latest",
+  apiKey: process.env.MISTRAL_API_KEY,
+});
 
-export async function testAi(){
-    geminiModel.invoke("Hello, how are you?").then((response) => {
-        console.log("AI Response:", response.text);
-    }).catch((error) => {
-        console.error("Error invoking AI model:", error);
-    });
+export async function generateResponse(message) {
+  const response = await geminiModel.invoke([
+    new HumanMessage(message),
+  ]);
+
+  return response.content;
 }
 
+
+export async function generateChatTitle(message) {
+
+    const response = await mistralModel.invoke([
+        new SystemMessage(`
+            You are a helpful assistant that generates concise and descriptive titles for chat conversations.
+            
+            User will provide you with the first message of a chat conversation, and you will generate a title that captures the essence of the conversation in 2-4 words. The title should be clear, relevant, and engaging, giving users a quick understanding of the chat's topic.    
+        `),
+        new HumanMessage(`
+            Generate a title for a chat conversation based on the following first message:
+            "${message}"
+            `)
+    ])
+
+    return response.text;
+
+}

28eab9d243da2d4fec457d0210fb16265b085f96
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-07-09 21:24:31 +0530

    Refactor chat controller for code consistency and formatting improvements

--- Backend/src/controllers/chat.controller.js
+++ Backend/src/controllers/chat.controller.js
@@ -1,6 +1,6 @@
-import { generateResponse  , generateChatTitle} from '../services/ai.service.js';
+import { generateResponse, generateChatTitle } from '../services/ai.service.js';
 import chatModel from '../models/chat.model.js';
-import messageModel from '../models/message.model.js';  
+import messageModel from '../models/message.model.js';
 
 
 
@@ -9,27 +9,31 @@ export async function sendMessage(req, res) {
     const { message } = req.body;
     const title = await generateChatTitle(message);
     const result = await generateResponse(message);
-    
+
 
     const chat = await chatModel.create({
-      user : req.user.id,
+      user: req.user.id,
       title
-      
-      
     });
 
-    const  aiMessage = await messageModel.create({
-      chat : chat._id,
-      content : result,
-      role : 'ai'
+    const userMessage = await messageModel.create({
+      chat: chat.id,
+      content: message,
+      role: "user"
+    })
+
+    const aiMessage = await messageModel.create({
+      chat: chat._id,
+      content: result,
+      role: 'ai'
     });
 
 
 
 
 
-    res.status(200).json({title , chat , aiMessage});
-    }catch (error) {
+    res.status(200).json({ title, chat, aiMessage });
+  } catch (error) {
     console.error('Error sending message:', error);
     res.status(500).json({ message: 'Internal server error' });
   }

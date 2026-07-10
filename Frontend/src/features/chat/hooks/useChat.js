c42c78177b6741006a17a21344bbef660a402e4d
Raj Kushwaha <rajkushwahacs105@gmail.com>
2026-07-10 23:17:35 +0530

    Add sendMessage functionality and initialize dispatch in useChat hook

--- Frontend/src/features/chat/hooks/useChat.js
+++ Frontend/src/features/chat/hooks/useChat.js
@@ -1,6 +1,16 @@
 import { initializeSocketConnection } from "../service/chat.socket"
+import { sendMessage  , getChats , getMessages , deleteChat } from '../service/chat.api';
+import { useDispatch } from "react-redux";
 
 export const useChat = () => {
+
+    const dispatch = useDispatch()
+
+    function handleSendMessage ({message , chatId}) {
+        
+    }
+
+
     return { 
         initializeSocketConnection,
     }
